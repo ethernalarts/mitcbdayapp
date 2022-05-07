cfeb646add1413582010daffc373e6d40b342463
Uwa Isibor <uwaisibor@pm.me>
2022-05-07 13:01:24 +0100

    updated bmessage_2.html

--- sendmail/views.py
+++ sendmail/views.py
@@ -48,7 +48,7 @@ def bdaycheck(request):
            
         time.sleep(2)        
         
-        t = loader.get_template('bmessage_1.html')
+        t = loader.get_template('bmessage_2.html')
         return HttpResponse(t.render(context = {'celebrants': celebrants, 'title': 'Birthdays Today'}))
     
         return sendmail(request, celebrants)
