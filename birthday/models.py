bfa7992c3b07b7e54b84a064aa1c0a77229429cb
Uwa Isibor <uwaisibor@pm.me>
2022-07-19 01:05:40 +0100

    1:04 PM 19/07/2022

--- birthday/models.py
+++ birthday/models.py
@@ -1,4 +1,5 @@
 
+import os
 import datetime
 from email.policy import default
 from enum import unique
@@ -12,40 +13,44 @@ from django.urls import reverse  # To generate URLS by reversing URL patterns
 # Create your models here.
 
 class staffDetails(models.Model):
-    """ Staff Details """
     
-    first_name = models.CharField('First Name', max_length=20, null=False)
+    first_name = models.CharField('First Name', max_length=20, null=False)    
+    middle_name = models.CharField('Middle Name', max_length=20, null=True, blank=True)    
+    last_name = models.CharField('Last Name', max_length=20, null=False)    
+    phone_number = PhoneNumberField(('Phone Number'), default='08012345678')    
+    email = models.EmailField('Official Email', max_length = 254, null=False)        
+    cadre = models.CharField(("Cadre"), max_length=50, null=False, default='Admin Officer')    
+    first_appointment = models.DateField(("Date of First Appointment"), default=datetime.date.today, null=False, blank=False)    
+    department = models.CharField('Department', max_length=100, null=False, default='Adminstration')    
+    level = models.IntegerField('Grade Level', null=False)    
+    step = models.IntegerField('Step', null=False)  
     
-    middle_name = models.CharField('Middle Name', max_length=20, null=True, blank=True)
+    def default_profile_pic(self):
+        if self.gender_text=='Female':
+            default = 'static/img/default-female.jpg'
+        elif self.gender_text=='Male':
+            default = 'static/img/default-male.jpg'
+        return default
     
-    last_name = models.CharField('Last Name', max_length=20, null=False)
+    staff_image = models.ImageField(("Profile Picture"), upload_to='staffimages', default='staffimages/default.jpg')
+    delete_image = models.BooleanField('Delete Profile Picture', default=0)    
     
-    phone_number = PhoneNumberField(('Phone Number'), default='08012345678')
+    # def deleteProfilePicture(self,*args,**kwargs):
+    #     if os.path.isfile(self.staff_image.path):
+    #         os.remove(self.staff_image.path)
+
+    #     super(staffDetails, self).delete(*args,**kwargs)
     
-    email = models.EmailField('Official Email', max_length = 254, null=False)   
-     
-    cadre = models.CharField(("Cadre"), max_length=50, null=False, default='Admin Officer')
-    
-    first_appointment = models.DateField(("Date of First Appointment"), default=datetime.date.today, null=False, blank=False)
-    
-    department = models.CharField('Department', max_length=100, null=False, default='Adminstration')
-    
-    level = models.IntegerField('Grade Level', null=False)
-    
-    step = models.IntegerField('Step', null=False)
-    
-    staff_image = models.ImageField(
-        verbose_name="Profile Picture",
-        upload_to='staffimages',
-        default='staffimages/default.jpg'
-    )
-    
+    def delete(self, using=None, keep_parents=False):
+        self.staff_image.storage.delete(self.staff_image.first_name)
+        super().delete()
+        
     GENDER = (
         (1, 'Male'),
         (2, 'Female'),
         (3, 'Prefer not to say')
     )
-    
+        
     gender = models.IntegerField('Gender', choices=GENDER, default=2)    
     
     def gender_text(self):
@@ -66,41 +71,33 @@ class staffDetails(models.Model):
         (12, 'December')
     )
     
-    birth_month = models.IntegerField('Birth Month', choices=MONTHS, default=1)
-    
-    birth_day = models.IntegerField('Birth Day', null=False)
-    
+    birth_month = models.IntegerField('Birth Month', choices=MONTHS, default=1)    
+    birth_day = models.IntegerField('Birth Day', null=False)    
     
     def birth_month_verbose(self):
-        return dict(staffDetails.MONTHS)[self.birth_month]
-    
+        return dict(staffDetails.MONTHS)[self.birth_month]    
     
     class Meta:
-        db_table = 'birthday_staffdetails'
-    
+        db_table = 'birthday_staffdetails'    
         
     def phone_number_default(self):
-        return staffDetails._meta.get_field('phone_number').get_default()
-    
+        return staffDetails._meta.get_field('phone_number').get_default()    
         
     @property
     def BIRTHDAY_TODAY(self):
         if (self.birth_month, self.birth_day) == (date.today().month, date.today().day):
             return True
-        return False
-    
+        return False    
     
     # Staff Details Reverse URL
     def get_absolute_url(self):
         """Returns the url to access a particular staff instance."""
-        return reverse('staffdetails', args=[str(self.id)])
-    
+        return reverse('staffdetails', args=[str(self.id)])    
     
     # Update Staff Reverse URL
     def get_absolute_url_update_staff(self):
         """Returns the url to update a particular staff instance."""
-        return reverse('updatestaff', args=[str(self.id)])
-    
+        return reverse('updatestaff', args=[str(self.id)])    
     
     def __str__(self):
         return "%s %s" % (self.first_name, self.last_name)
